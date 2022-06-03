9eb50a5223f0550687e5650bc3c21e77f47faf73
Hyzer Official <98345064+Hyzerr@users.noreply.github.com>
2022-06-03 17:17:41 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -12,7 +12,7 @@ let esce = `
 Hai ${ye} Lagi Nyari Sc Ya? 
 
 Nih Link Sc Nya Dibawah :
-https://youtube.com/channel/UCBtUyjfIclyuu7yXKS0dAMw
+https://youtu.be/AXPzjQ5jxx8
 
 _*Jangan Lupa Di Subscribe*_`
 conn.sendBut(m.chat, esce, wm3, 'Thanks', 'thanks', m) 
