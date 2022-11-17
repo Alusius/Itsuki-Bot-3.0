4ae2ebb74a21730e36b0b21fab1e8038d92f447c
Hyzer Official <98345064+Hyzerr@users.noreply.github.com>
2022-11-17 17:08:01 +0700

    Create sc.js

---
+++ plugins/info/sc.js
@@ -0,0 +1,11 @@
+const handler = async (msg, { client }) => {
+msg.reply('https://github.com/Hyzerr/Hyzer-MD')
+}
+handler.tags = ['info']
+handler.help = ['sc','sourcecode','script']
+handler.command = /^(sc|script|sourcecode)$/i
+
+module.exports = handler
+
+// jangan diubah kontol
+// jgn diklaim ajg
