98dd64a17303dca81c34e7b1193935de534f34b2
Hyzer Official <98345064+Hyzerr@users.noreply.github.com>
2022-11-18 14:57:30 +0700

    fix quoted

--- plugins/converter/stiker.js
+++ plugins/converter/stiker.js
@@ -3,20 +3,21 @@ const fs = require('fs')
 var handler = async (msg, { 
 client, args, usedPrefix, command 
 }) => {
-var q = msg.quoted ? msg.quoted : msg
-var mime = (q.msg || q).mimetype || ''
-var mimeq = (q.msg || q)
+const zer = (msg.quoted || msg)
+const quoted = (zer.mtype == 'buttonsMessage') ? zer[Object.keys(zer)[1]] : (zer.mtype == 'templateMessage') ? zer.hydratedTemplate[Object.keys(zer.hydratedTemplate)[1]] : (zer.mtype == 'product') ? zer[Object.keys(zer)[0]] : msg.quoted ? msg.quoted : msg
+const mime = (quoted.hyzerMess || quoted).mimetype || ''
+const qmsg = (quoted.hyzerMess || quoted)
 if (/image/.test(mime)) {
-var image = await client.downloadMediaMessage(mimeq)
+var image = await client.downloadMediaMessage(qmsg)
 var encimage = await client.sendImageAsSticker(msg.from, image, msg, { packname: args[0] || Info.sticker.packname, author: args[1] || Info.sticker.author })
 await fs.unlinkSync(encimage)
 } else if (/video/.test(mime)) {
 if (mimeq.seconds > 11) return msg.reply('🚩 Maksimal 10 detik!')
-var video = await client.downloadMediaMessage(mimeq)
+var video = await client.downloadMediaMessage(qmsg)
 var encvideo = await client.sendVideoAsSticker(msg.from, video, msg, { packname: args[0] || Info.sticker.packname, author: args[1] || Info.sticker.author })
 await fs.unlinkSync(encvideo)
 } else {
-msg.reply(`🚩 Kirim/reply gambar/video/gif dengan caption ${usedPrefix + command}\nDurasi Video/Gif 1-9 Detik`)
+msg.reply(`🚩 Kirim gambar/video dengan caption ${usedPrefix + command}\nDurasi Video/Gif 1-9 Detik`)
 }
 }
 handler.help = ['sticker','s','sgif','stickergif'].map(v => v + ' <reply image>')
