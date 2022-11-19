d4b8b58852e5a578d8afa4fb55f127fe34b46814
Hyzer Official <98345064+Hyzerr@users.noreply.github.com>
2022-11-20 05:42:19 +0700

    Create my.js

---
+++ plugins/exp/my.js
@@ -0,0 +1,18 @@
+var handler = async(msg, { 
+client 
+}) => {
+var teks = `*乂  L I M I T  &  M O N E Y*
+
+  *◦ Limit :* ${user.limit}
+  *◦ Money :* Rp ${Func.toRupiah(user.money)}
+  *◦ Poin :* ${user.point}
+  *◦ Premium :* [ *${Info.premium.includes(msg.sender.split('@')[0]) ? '√' : '×'}* ]
+`
+var buton = await Func.satuButton('.menu','Menu')
+client.reply(msg.from, teks, msg, { buttons: buton, headerType: 5, footer: wm })
+}
+handler.help = ['my','limit']
+handler.tags = ['xp']
+handler.command = /^(my|limit)$/i
+
+module.exports = handler
