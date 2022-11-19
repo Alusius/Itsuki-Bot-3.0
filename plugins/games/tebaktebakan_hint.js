c6f0138f654309772dceed3bfd1c8c023fa58c3a
NevtGanz <95397046+NevtBotz@users.noreply.github.com>
2022-11-19 08:43:12 +0800

    Update tebaktebakan_hint.js

--- plugins/games/tebaktebakan_hint.js
+++ plugins/games/tebaktebakan_hint.js
@@ -1,13 +1,13 @@
 var handler = async (msg, { 
 client 
 }) => {
-client.asahotak = client.asahotak ? client.asahotak : {}
+client.tebaktebakan = client.tebaktebakan ? client.tebaktebakan : {}
 var id = msg.from
-if (!(id in client.asahotak)) return false
-var json = client.asahotak[id][1]
+if (!(id in client.tebaktebakan)) return false
+var json = client.tebaktebakan[id][1]
 var ans = json.jawaban.trim()
 var clue = ans.replace(/[AIUEOaiueo]/g, '_')
-client.reply(msg.from, '```' + clue + '```\nBalas soalnya, bukan pesan ini', client.asahotak[id][0])
+client.reply(msg.from, '```' + clue + '```\nBalas soalnya, bukan pesan ini', client.tebaktebakan[id][0])
 }
 handler.command = /^tekod$/i
 handler.limit = true
