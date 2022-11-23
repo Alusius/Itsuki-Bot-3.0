124781d8b21d1bf842bce35cdd76d8f993189a7d
Hyzer Official <98345064+Hyzerr@users.noreply.github.com>
2022-11-24 03:33:53 +0700

    Piece 部

--- plugins/exp/daftarprem.js
+++ plugins/exp/daftarprem.js
@@ -7,7 +7,7 @@ await msg.reply('🚩 Berhasil menjadi anggota premium bot secara permanen, keti
 user.money -= 500000000 * 1
 Info.premium.push(msg.sender.split('@')[0])
 } else {
-msg.reply('Untuk mendaftar menjadi *pengguna premium* menggunakan inventory, kamu harus memiliki money sebesar\nRp 500.000.000.')
+msg.reply('Untuk mendaftar menjadi *pengguna premium* menggunakan inventory, kamu harus memiliki money sebesar\n\nRp 500.000.000.')
 }
 }
 handler.help = ['daftarprem','daftarpremium']
