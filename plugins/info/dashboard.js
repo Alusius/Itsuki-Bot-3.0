ea24c105217af3c08c6b7494bc19134ed56554c5
Hyzer Official <98345064+Hyzerr@users.noreply.github.com>
2022-11-23 10:51:48 +0700

    piece

--- plugins/info/dashboard.js
+++ plugins/info/dashboard.js
@@ -4,7 +4,7 @@ client, usedPrefix
 var listHit = []
 for (let i of hitBot) {
 listHit.push({
-title: `𝗖𝗼𝗺𝗺𝗮𝗻𝗱 : ${usedPrefix + i.id}`, rowId: `${usedPrefix + i.id}`, description: `𝗣𝗲𝗻𝗴𝗴𝘂𝗻𝗮𝗮𝗻 : ${i.total}x`})
+title: `Fitur : ${i.id}`, rowId: `${usedPrefix + i.id}`, description: `Penggunaan : ${i.total} Kali`})
 }
 const sections = [{
 title: "DAFTAR TOTAL PENGGUNAAN FITUR BOT",
@@ -12,8 +12,6 @@ rows: listHit
 }]
 const listMessage = {
 text: '🚩 Berikut adalah total penggunaan fitur bot, pilih fitur yang ingin digunakan disini.',
-footer: Info.botWm,
-title: `*乂 D A S H B O A R D - B O T*\n`,
 buttonText: "Daftar Penggunaan Fitur",
 sections
 }
