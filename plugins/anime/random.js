fc53e757a73cd23ab896fb6311368ab0cd34ed68
Hyzer Official <98345064+Hyzerr@users.noreply.github.com>
2022-11-23 21:58:03 +0700

    Piece 部

--- plugins/anime/random.js
+++ plugins/anime/random.js
@@ -1,16 +1,16 @@
 var fetch = require('node-fetch')
 
 var handler = async(msg, { 
-client, command 
+client,
 }) => {
-var buton = await Func.satuButton(`.${command}`, 'NEXT')
-var imge = await fetch('https://raw.githubusercontent.com/Hyzerr/Database/master/Database/Anime/' + command + '.json')
+var buton = await Func.satuButton(`.loli`, 'NEXT')
+var imge = await fetch('https://raw.githubusercontent.com/Hyzerr/Database/master/Database/Anime/loli.json')
 var img = await imge.json()
 var res = Func.pickRandom(img)
 client.sendImage(msg.from, res, '🚩 Generate random image ' + command, msg, { isUrl: true, buttons: buton, headerType: 5, footer: wm })
 }
-handler.help = ['akira','akiyama','ana','asuna','ayuzawa','boruto','chitanda','chitoge','deidara','elaina','emilia','erza','gremory','hestia','hinata','inori','isuzu','itachi','itori','kaga','kagura','kakasih','kaneki','kaori','kosaki','kotori','kuriyama','kuroha','kurumi','loli','madara','megumin','mikasa','miku','minato','naruto','natsukawa','neko','nekonime','nezuko','nishimiya','onepiece','rem','rize','sagiri','sakura','sasuke','shina','shinka','shizuka','shota','tomori','toukachan','tsunade','yatogami','yuki']
+handler.help = ['loli']
 handler.tags = ['anime']
-handler.command = /^(akira|akiyama|ana|asuna|ayuzawa|boruto|chitanda|chitoge|deidara|elaina|emilia|erza|gremory|hestia|hinata|inori|isuzu|itachi|itori|kaga|kagura|kakasih|kaneki|kaori|kosaki|kotori|kuriyama|kuroha|kurumi|loli|madara|megumin|mikasa|miku|minato|naruto|natsukawa|neko|nekonime|nezuko|nishimiya|onepiece|rem|rize|sagiri|sakura|sasuke|shina|shinka|shizuka|shota|tomori|toukachan|tsunade|yatogami|yuki)$/i
+handler.command = /^(loli)$/i
 
 module.exports = handler
