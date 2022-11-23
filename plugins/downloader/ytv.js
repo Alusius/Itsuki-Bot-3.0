5d054129cc56852426a8770c55a395467a6a8bbd
Hyzer Official <98345064+Hyzerr@users.noreply.github.com>
2022-11-24 03:31:36 +0700

    Piece 部

--- plugins/downloader/ytv.js
+++ plugins/downloader/ytv.js
@@ -4,7 +4,7 @@ client, text, usedPrefix, command
 if (!text) return msg.reply(`🚩 Gunakan format ${usedPrefix + command} *<link-youtube>*\n\n*Contoh :* ${usedPrefix + command} https://youtu.be/Db3QI5OUO5o`)
 client.sendReact(msg.from, '🕒', msg.key)
 require('caliph-api').downloader.yt.mp4(text).then(async v => {
-client.sendVideo(msg.from, v.result.result, `*乂 Y O U T U B E - V I D E O*\n\n   *◦ Title :* ${v.result.title}\n   *◦ Quality :* ${v.result.quality}\n   *◦ Size :* ${v.result.size}\n   *◦ Duration :* ${v.result.duration}\n   *◦ Channel :* ${v.result.channel}`, msg, { isUrl:true })
+client.sendVideo(msg.from, v.result.result, `*乂 Y O U T U B E - V I D E O*\n\n   *◦ Title :* ${v.result.title}\n   *◦ Quality :* ${v.result.quality}\n   *◦ Duration :* ${v.result.duration}\n   *◦ Channel :* ${v.result.channel}`, msg, { isUrl:true })
 })
 }
 handler.help = ['ytv','ytmp4']
