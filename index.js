c7f78aae91513f79cbd2f9e974f4f13ed85f3253
Hyzer Official <98345064+Hyzerr@users.noreply.github.com>
2022-11-27 16:06:10 +0700

    Piece 部

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-var cluster = require('cluster')
-var path = require('path')
-var fs = require('fs')
-var package = require('./package.json')
-var CFonts = require('cfonts')
-var Readline = require('readline')
-var yargs = require('yargs/yargs')
-var rl = Readline.createInterface(process.stdin, process.stdout)
+console.log('Starting...')
+let cluster = require('cluster')
+let path = require('path')
+let fs = require('fs')
+let package = require('./package.json')
+const CFonts = require('cfonts')
+const Readline = require('readline')
+const yargs = require('yargs/yargs')
+const rl = Readline.createInterface(process.stdin, process.stdout)
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 
-CFonts.say('Simple WhatsApp Bot By Hyzer Official', {
-    font: 'console',
-    align: 'center',
-    gradient: ['red', 'magenta']
+CFonts.say('BOT BY\n Tio', {
+  colors: ['blueBright','yellowBright'],                                        font: 'block',
+  align: 'center',
+})
+CFonts.say(`BY TioXd`, {                                            
+colors: ['yellow'],                                                           
+font: 'console',                                                              align: 'center',
 })
 
 var isRunning = false
@@ -21,13 +26,19 @@ var isRunning = false
 function start(file) {
   if (isRunning) return
   isRunning = true
-  var args = [path.join(__dirname, file), ...process.argv.slice(2)]  
+  let args = [path.join(__dirname, file), ...process.argv.slice(2)]
+  CFonts.say([process.argv[0], ...args].join(' '), {
+    font: 'console',
+    align: 'center',
+    gradient: ['red', 'magenta']
+  })
   cluster.setupMaster({
     exec: path.join(__dirname, file),
     args: args.slice(1),
   })
-  var p = cluster.fork()
+  let p = cluster.fork()
   p.on('message', data => {
+    console.log('[RECEIVED]', data)
     switch (data) {
       case 'reset':
         p.kill()
@@ -41,13 +52,14 @@ function start(file) {
   })
   p.on('exit', code => {
     isRunning = false
+    console.error('Exited with code:', code)
     if (code === 0) return
     fs.watchFile(args[0], () => {
       fs.unwatchFile(args[0])
       start(file)
     })
   })
-  var opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
+  let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
   if (!opts['test'])
     if (!rl.listenerCount()) rl.on('line', line => {
       p.emit('message', line.trim())
